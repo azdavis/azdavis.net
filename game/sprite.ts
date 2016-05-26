4cde11711eeb1f50d07467ad571c84fb605e469e
azdavis <ariel.z.davis@icloud.com>
2016-05-26 01:03:43 -0400

    game/sprite: add speed, width, height

--- game/sprite.ts
+++ game/sprite.ts
@@ -1,7 +1,13 @@
 abstract class Sprite {
     elem: HTMLDivElement
+    speed: number
 
-    constructor(public x: number, public y: number) {
+    constructor(
+        public x: number,
+        public y: number,
+        public width: number,
+        public height: number
+    ) {
         this.elem = document.createElement('div')
         this.elem.className = 'sprite'
         this.setStyle()
@@ -9,6 +15,16 @@ abstract class Sprite {
     }
 
     move(dx: number, dy: number): void {
+        if (
+            this.x + dx <= 0 ||
+            this.x + dx >= window.innerWidth - this.width
+        )
+            dx = 0
+        if (
+            this.y + dy <= 0 ||
+            this.y + dy >= window.innerHeight - this.height
+        )
+            dy = 0
         if (dx == 0 && dy == 0)
             return
         this.x += dx
