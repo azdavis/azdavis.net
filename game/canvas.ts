87f3bf78fc43adb4781f5c7426f0430fecdf7df2
azdavis <ariel.z.davis@icloud.com>
2016-05-26 13:09:12 -0400

    game/canvas: add

---
+++ game/canvas.ts
@@ -0,0 +1,37 @@
+import Sprite from './sprite'
+
+class Canvas {
+    elem: HTMLCanvasElement
+    ctx: CanvasRenderingContext2D
+    ratio = window.devicePixelRatio || 1
+
+    constructor() {
+        this.elem = document.createElement('canvas')
+        this.ctx = this.elem.getContext('2d')
+        this.resize()
+        document.body.appendChild(this.elem)
+    }
+
+    resize(): void {
+        let w = window.innerWidth
+        let h = window.innerHeight
+        this.elem.width = w * this.ratio
+        this.elem.height = h * this.ratio
+        this.elem.style.width = w + 'px'
+        this.elem.style.height = h + 'px'
+        this.ctx.scale(this.ratio, this.ratio)
+    }
+
+    draw(s: Sprite): void {
+        this.ctx.fillStyle = s.fill
+        this.ctx.fillRect(s.x, s.y, s.width, s.height)
+        this.ctx.strokeStyle = s.stroke
+        this.ctx.strokeRect(s.x, s.y, s.width, s.height)
+    }
+
+    clear(): void {
+        this.ctx.clearRect(0, 0, this.elem.width, this.elem.height)
+    }
+}
+
+export default Canvas
