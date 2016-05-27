4059a9aafeb5bd79c5955bdcb5dca5d49987b7f3
azdavis <ariel.z.davis@icloud.com>
2016-05-27 01:43:28 -0400

    game/player: shoot bullets

--- game/player.ts
+++ game/player.ts
@@ -1,40 +1,21 @@
 import Sprite from './sprite'
+import Bullet from './bullet'
 
 class Player extends Sprite {
+    width = 50
+    height = 50
+    bullets: Bullet[] = []
     speed = 1
-    private hyp = Math.sqrt(2 * Math.pow(this.speed, 2))
     fill = '#4b4'
-    dir = {
-        lt: false,
-        up: false,
-        rt: false,
-        dn: false
-    }
 
     constructor() {
         super(0, 0)
     }
 
-    move(): void {
-        let dx = 0
-        let dy = 0
-        if (this.dir.lt)
-            dx -= this.speed
-        if (this.dir.up)
-            dy -= this.speed
-        if (this.dir.rt)
-            dx += this.speed
-        if (this.dir.dn)
-            dy += this.speed
-        if (this.x + dx < 0 || this.x + dx > window.innerWidth - this.width)
-            dx = 0
-        if (this.y + dy < 0 || this.y + dy > window.innerHeight - this.height)
-            dy = 0
-        if (dx && !dy)
-            dx *= this.hyp
-        if (dy && !dx)
-            dy *= this.hyp
-        super.move(dx, dy)
+    shoot(): void {
+        if (this.i == 0 && this.j == 0)
+            return
+        this.bullets.push(new Bullet(this))
     }
 }
 
