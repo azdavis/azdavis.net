a2380affcbf4cbf60c7e1b28b146351b953978dd
azdavis <ariel.z.davis@icloud.com>
2016-05-27 01:44:19 -0400

    game/index: handle pressed keys better

--- game/index.ts
+++ game/index.ts
@@ -3,30 +3,37 @@ import Game from './game'
 const game = new Game()
 
 const keys = {
+    sp: 32,
     lt: 37,
     up: 38,
     rt: 39,
     dn: 40,
     p: 80
 }
+const pressed = {}
 
 onkeydown = (e) => {
+    if (pressed[e.which])
+        return
     switch (e.which) {
-    case keys.lt: game.player.dir.lt = true; break
-    case keys.up: game.player.dir.up = true; break
-    case keys.rt: game.player.dir.rt = true; break
-    case keys.dn: game.player.dir.dn = true; break
-    case keys.p: game.toggle()
+    case keys.sp: game.player.shoot(); break
+    case keys.lt: game.player.i -= 1; break
+    case keys.up: game.player.j -= 1; break
+    case keys.rt: game.player.i += 1; break
+    case keys.dn: game.player.j += 1; break
+    case keys.p: game.toggle(); break
     }
+    pressed[e.which] = true
 }
 
 onkeyup = (e) => {
     switch (e.which) {
-    case keys.lt: game.player.dir.lt = false; break
-    case keys.up: game.player.dir.up = false; break
-    case keys.rt: game.player.dir.rt = false; break
-    case keys.dn: game.player.dir.dn = false; break
+    case keys.lt: game.player.i += 1; break
+    case keys.up: game.player.j += 1; break
+    case keys.rt: game.player.i -= 1; break
+    case keys.dn: game.player.j -= 1; break
     }
+    pressed[e.which] = false
 }
 
 onresize = () => {
