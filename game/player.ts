63b8c10c2720055d63caab3636a03bd6afe19a4d
azdavis <ariel.z.davis@icloud.com>
2016-06-11 15:32:21 -0400

    game: make maxAmmo have only 1 digit

--- game/player.ts
+++ game/player.ts
@@ -8,7 +8,7 @@ class Player extends Sprite {
     public fill = '#4b4'
     public bullets: Bullet[]
     public maxLives = 3
-    public maxAmmo = 10
+    public maxAmmo = 9
     public lives: number
     public ammo: number
     public dirs = {
