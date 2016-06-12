648065c12f960d96f593586735fa790a30f95165
azdavis <ariel.z.davis@icloud.com>
2016-06-12 18:57:09 -0400

    game/bullet: close #1

--- game/bullet.ts
+++ game/bullet.ts
@@ -7,11 +7,24 @@ class Bullet extends Sprite {
     public fill = '#44b'
 
     // make this always move in the direction that s was going in when this was
-    // made, and start it in the center of s
+    // made, and start it on the edge or corner of s facing the direction of
+    // movement
     public constructor(s: Sprite) {
         super(s.x, s.y)
-        this.x += (s.w - this.w) / 2
-        this.y += (s.h - this.h) / 2
+        let dx = (s.w - this.w) / 2
+        let dy = (s.h - this.h) / 2
+        if (s.i >= 0) {
+            this.x += dx
+        }
+        if (s.i > 0) {
+            this.x += dx
+        }
+        if (s.j >= 0) {
+            this.y += dy
+        }
+        if (s.j > 0) {
+            this.y += dy
+        }
         this.i = s.i
         this.j = s.j
     }
