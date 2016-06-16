bbbc74f8b2bf05dbae18f57761a48db827083c10
azdavis <ariel.z.davis@icloud.com>
2016-06-15 23:50:43 -0400

    index: enter a night mode when it's late

--- index.ts
+++ index.ts
@@ -1,10 +1,27 @@
 const rel = <NodeListOf<HTMLAnchorElement>>document.querySelectorAll("a")
 const http = /https?:\/\//
-let i
+
+let i: number
 for (i = 0; i < rel.length; i++) {
     if (rel[i].href.match(http)) {
         rel[i].innerHTML = rel[i].href.replace(http, "")
     }
 }
 
-document.querySelector("#now").innerHTML = "" + new Date().getFullYear()
+const now = <HTMLElement>document.querySelector("#now")
+const html = document.documentElement
+
+function doDateThings(): void {
+    const d = new Date()
+    const hr = d.getHours()
+    now.innerHTML = "" + d.getFullYear()
+    if (hr <= 5 || hr >= 18) {
+        html.style.backgroundColor = "#222"
+        html.style.color = "#ddd"
+    } else {
+        html.style.backgroundColor = html.style.color = ""
+    }
+}
+
+doDateThings()
+setInterval(doDateThings, 10000)
