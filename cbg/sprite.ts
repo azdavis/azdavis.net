800f72cb47e3a46f5c2d093ab1d1f3b5754014d9
azdavis <ariel.z.davis@icloud.com>
2016-06-28 15:38:07 -0400

    cbg: make sprite an abstract class

--- cbg/sprite.ts
+++ cbg/sprite.ts
@@ -1,9 +1,15 @@
-interface Sprite {
-    x: number
-    y: number
-    fill: string
-    stroke: string
-    draw(): void
+import Canvas from "./canvas"
+
+abstract class Sprite {
+    protected x: number
+    protected y: number
+    protected fill: string
+    protected stroke: string
+
+    protected draw(): void {
+        Canvas.ctx.fillStyle = this.fill
+        Canvas.ctx.strokeStyle = this.stroke
+    }
 }
 
 export default Sprite
