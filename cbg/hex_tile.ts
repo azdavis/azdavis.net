3b6d5c248b260636c6ac1ff4eff5be0e80c09da4
azdavis <ariel.z.davis@icloud.com>
2016-06-28 15:38:57 -0400

    cbg: start implementing Sprite in subclasses

--- cbg/hex_tile.ts
+++ cbg/hex_tile.ts
@@ -1,6 +1,20 @@
+import Canvas from "./canvas"
 import NumberLabel from "./number_label"
 import Sprite from "./sprite"
 
-class HexTile implements Sprite {}
+class HexTile extends Sprite {
+    protected x: number
+    protected y: number
+    protected fill: string
+    protected stroke: string
+    private sideLength = 30
+    private label: NumberLabel
+
+    public constructor() {
+        super()
+    }
+
+    protected draw(): void {}
+}
 
 export default HexTile
--- cbg/number_label.ts
+++ cbg/number_label.ts
@@ -1,5 +1,30 @@
+import Canvas from "./canvas"
 import Sprite from "./sprite"
 
-class NumberLabel implements Sprite {}
+class NumberLabel extends Sprite {
+    protected x: number
+    protected y: number
+    protected fill = "#ddd"
+    protected stroke: string
+    private radius = 10
+
+    public constructor(private n: number) {
+        super()
+        this.x = 0
+        this.y = 0
+        this.stroke =
+            n === 6 || n === 8 ?
+            "#a22" :
+            "#222"
+    }
+
+    protected draw(): void {
+        super.draw()
+        Canvas.ctx.beginPath()
+        Canvas.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+        Canvas.ctx.fill()
+        Canvas.ctx.stroke()
+    }
+}
 
 export default NumberLabel
