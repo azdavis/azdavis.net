286dd3a603b7d861c671b2136f91799e1f6f7b0e
azdavis <ariel.z.davis@icloud.com>
2016-06-28 20:30:29 -0400

    cbg/catan: reorganize a bit
    hopefully this makes a bit more sense.

--- cbg/catan.ts
+++ cbg/catan.ts
@@ -1,42 +1,53 @@
 namespace Catan {
-    export const enum TileType {
-        Brick,
-        Wood,
-        Wheat,
-        Sheep,
-        Ore,
-        Desert,
+    export const counters = {
+        infrequent: [2, 12],
+        regular: [3, 4, 5, 9, 10, 11],
+        frequent: [6, 8],
+    }
+    counters.notInfrequent = counters.regular.concat(counters.frequent)
+
+    export const enum resources {
+        brick,
+        wood,
+        wheat,
+        sheep,
+        ore,
+        desert,
     }
 
     export const regular = {
         rows: [1, 2, 3, 2, 3, 2, 3, 2, 1],
-        resourceCounts: {
-            Brick: 3,
-            Wood: 4,
-            Wheat: 4,
-            Sheep: 4,
-            Ore: 3,
-            Desert: 1,
-        },
-        numbers: {
-            1: [2, 12],
-            2: [3, 4, 5, 6, 8, 9, 10, 11],
+        amts: {
+            resources: {
+                brick: 3,
+                wood: 4,
+                wheat: 4,
+                sheep: 4,
+                ore: 3,
+                desert: 1,
+            },
+            counters: {
+                infrequent: 1,
+                notInfrequent: 2,
+            },
         },
     }
 
     export const expansion = {
         rows: [1, 2, 3, 4, 3, 4, 3, 4, 3, 2, 1],
-        resourceCounts: {
-            Brick: 5,
-            Wood: 6,
-            Wheat: 6,
-            Sheep: 6,
-            Ore: 5,
-            Desert: 2,
-        },
-        numbers: {
-            2: [2, 12],
-            3: [3, 4, 5, 6, 8, 9, 10, 11],
+        amts: {
+            resources: {
+                brick: 5,
+                wood: 6,
+                wheat: 6,
+                sheep: 6,
+                ore: 5,
+                desert: 2,
+            },
+            counters: {
+                infrequent: 2,
+                notInfrequent: 3,
+            },
         },
     }
 }
