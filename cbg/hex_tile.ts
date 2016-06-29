28f89e4cf03bfc9618caddc8904608a264decff4
azdavis <ariel.z.davis@icloud.com>
2016-06-28 20:32:48 -0400

    cbg: implement Sprite more in subclasses

--- cbg/hex_tile.ts
+++ cbg/hex_tile.ts
@@ -3,18 +3,31 @@ import NumberLabel from "./number_label"
 import Sprite from "./sprite"
 
 class HexTile extends Sprite {
+    private static side = 30
     protected x: number
     protected y: number
     protected fill: string
-    protected stroke: string
-    private sideLength = 30
-    private label: NumberLabel
 
-    public constructor() {
-        super()
+    public constructor(private label: NumberLabel) {
+        super(0, 0)
     }
 
-    protected draw(): void {}
+    protected draw(): void {
+        const sideHalf = HexTile.side / 2
+        const xOff = sideHalf * Math.sqrt(3)
+        Canvas.ctx.fillStyle = this.fill
+
+        Canvas.ctx.moveTo(this.x, this.y - HexTile.side)
+        Canvas.ctx.beginPath()
+        Canvas.ctx.lineTo(this.x + xOff, this.y - sideHalf)
+        Canvas.ctx.lineTo(this.x + xOff, this.y + sideHalf)
+        Canvas.ctx.lineTo(this.x, this.y + HexTile.side)
+        Canvas.ctx.lineTo(this.x - xOff, this.y + sideHalf)
+        Canvas.ctx.lineTo(this.x - xOff, this.y - sideHalf)
+
+        Canvas.ctx.closePath()
+        Canvas.ctx.fill()
+    }
 }
 
 export default HexTile
--- cbg/number_label.ts
+++ cbg/number_label.ts
@@ -2,28 +2,24 @@ import Canvas from "./canvas"
 import Sprite from "./sprite"
 
 class NumberLabel extends Sprite {
+    private static radius = 10
     protected x: number
     protected y: number
     protected fill = "#ddd"
-    protected stroke: string
-    private radius = 10
 
     public constructor(private n: number) {
-        super()
-        this.x = 0
-        this.y = 0
-        this.stroke =
+        super(0, 0)
+        this.fill =
             n === 6 || n === 8 ?
             "#a22" :
             "#222"
     }
 
     protected draw(): void {
-        super.draw()
-        Canvas.ctx.beginPath()
-        Canvas.ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI)
+        Canvas.ctx.fillStyle = this.fill
+        Canvas.ctx.arc(this.x, this.y, NumberLabel.radius, 0, 2 * Math.PI)
         Canvas.ctx.fill()
-        Canvas.ctx.stroke()
+        Canvas.ctx.fillText(String(this.n), this.x, this.y)
     }
 }
 
