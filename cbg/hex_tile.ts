1cde5139931f7af13bcb8c564b8d398a5b99b1a2
azdavis <ariel.z.davis@icloud.com>
2016-06-30 13:33:22 -0400

    cbg/hex_tile: use private static to cache numbers

--- cbg/hex_tile.ts
+++ cbg/hex_tile.ts
@@ -4,6 +4,8 @@ import Sprite from "./sprite"
 
 class HexTile extends Sprite {
     private static side = 30
+    private static halfSide = HexTile.side / 2
+    private static xLeg = HexTile.halfSide * Math.sqrt(3)
     protected x: number
     protected y: number
     protected fill: string
@@ -14,17 +16,15 @@ class HexTile extends Sprite {
 
     // draw a hexagon, centered at (this.x, this.y)
     protected draw(): void {
-        const sideHalf = HexTile.side / 2
-        const xOff = sideHalf * Math.sqrt(3)
         Canvas.ctx.fillStyle = this.fill
 
         Canvas.ctx.moveTo(this.x, this.y - HexTile.side)
         Canvas.ctx.beginPath()
-        Canvas.ctx.lineTo(this.x + xOff, this.y - sideHalf)
-        Canvas.ctx.lineTo(this.x + xOff, this.y + sideHalf)
+        Canvas.ctx.lineTo(this.x + HexTile.xLeg, this.y - HexTile.halfSide)
+        Canvas.ctx.lineTo(this.x + HexTile.xLeg, this.y + HexTile.halfSide)
         Canvas.ctx.lineTo(this.x, this.y + HexTile.side)
-        Canvas.ctx.lineTo(this.x - xOff, this.y + sideHalf)
-        Canvas.ctx.lineTo(this.x - xOff, this.y - sideHalf)
+        Canvas.ctx.lineTo(this.x - HexTile.xLeg, this.y + HexTile.halfSide)
+        Canvas.ctx.lineTo(this.x - HexTile.xLeg, this.y - HexTile.halfSide)
 
         Canvas.ctx.closePath()
         Canvas.ctx.fill()
