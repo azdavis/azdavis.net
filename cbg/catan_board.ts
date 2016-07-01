21150dddc90b51b68be308e826d80ebe89871fff
azdavis <ariel.z.davis@icloud.com>
2016-07-01 02:19:45 -0400

    cbg/catan_board: implement fillRows()

--- cbg/catan_board.ts
+++ cbg/catan_board.ts
@@ -1,6 +1,8 @@
 import Canvas from "./canvas"
 import CatanData from "./catan_data"
 import HexTile from "./hex_tile"
+import NumberLabel from "./number_label"
+import rp from "./repeat_primitive"
 
 namespace CatanBoard {
     let rows: HexTile[][]
@@ -23,11 +25,42 @@ namespace CatanBoard {
         }
     }
 
+    // returns a random integer in the interval [0, h) (note that this will
+    // never return h)
+    function randInt(h: number): number {
+        return Math.floor(Math.random() * h)
     }
 
-    // fill the rows array by randomly assigning each HexTile
+    // fill the rows array by randomly assigning space to a HexTile with a
+    // random background and a random label
     function fillRows(): void {
+        const numLabels = CatanData.labels.length
+        const numTiles = CatanData.tiles.length
+
+        const labels = rp(false, numLabels)
+        const tiles = rp(false, numTiles)
+
+        let l: number
+        let t: number
+
+        // for each row...
         for (let i = 0; i < rows.length; i++) {
+            // for each space in the row...
+            for (let j = 0; j < rows[i].length; j++) {
+                // get a random label and tile type...
+                do { l = randInt(numLabels) } while (labels[l])
+                do { t = randInt(numTiles) } while (tiles[t])
+                labels[l] = tiles[t] = false
+                // set the space to a tile of the gotten type...
+                rows[i][j] = new HexTile(
+                    CatanData.fills.tiles[CatanData.tiles[t]],
+                    // with a label of the gotten number
+                    new NumberLabel(
+                        CatanData.fills.labels[CatanData.labels[l]],
+                        CatanData.labels[l]
+                    )
+                )
+            }
         }
     }
 
