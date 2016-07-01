f8487fb76a22f34608fd5fb641618c7222132f6b
azdavis <ariel.z.davis@icloud.com>
2016-07-01 02:18:11 -0400

    cbg/number_label: use background and text fills

--- cbg/number_label.ts
+++ cbg/number_label.ts
@@ -3,19 +3,20 @@ import Sprite from "./sprite"
 
 class NumberLabel extends Sprite {
     private static radius = 10
+    private static backgroundFill = "#ddd"
     protected x: number
     protected y: number
-    protected fill: string
 
-    public constructor(private n: number) {
+    public constructor(protected fill: string, private n: number) {
         super(0, 0)
     }
 
     // draw a circle with a number in the center
     protected draw(): void {
-        Canvas.ctx.fillStyle = this.fill
         Canvas.ctx.arc(this.x, this.y, NumberLabel.radius, 0, 2 * Math.PI)
+        Canvas.ctx.fillStyle = NumberLabel.backgroundFill
         Canvas.ctx.fill()
+        Canvas.ctx.fillStyle = this.fill
         Canvas.ctx.fillText(String(this.n), this.x, this.y)
     }
 }
