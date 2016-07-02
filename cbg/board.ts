36251ae37bd5feff2f32c82a9841c232266ed9fc
azdavis <ariel.z.davis@icloud.com>
2016-07-02 13:00:30 -0400

    cbg/board: add whitespace

--- cbg/board.ts
+++ cbg/board.ts
@@ -14,10 +14,12 @@ namespace Board {
         Canvas.clear()
         // for each row
         for (let i = 0; i < rows.length; i++) {
-            // for each tile in the row
             const dy = (i - (rows.length - 1) / 2) * HexTile.a * 1.1
+
+            // for each tile in the row
             for (let j = 0; j < rows[i].length; j++) {
                 const dx = ((rows[i].length - 1) / 2 - j) * HexTile.c * 3.3
+
                 rows[i][j].draw(
                     Canvas.center.x + dx,
                     Canvas.center.y + dy
