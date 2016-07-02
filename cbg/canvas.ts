ca5b182f55f5353f41129d9008e6e6a0aa926542
azdavis <ariel.z.davis@icloud.com>
2016-07-02 16:05:33 -0400

    cbg/canvas: resize better

--- cbg/canvas.ts
+++ cbg/canvas.ts
@@ -1,24 +1,27 @@
+import Data from "./data"
+import HexTile from "./hex_tile"
+
 namespace Canvas {
     export const el = document.querySelector("canvas") as HTMLCanvasElement
     export const cx = el.getContext("2d")
-    export const center = {x: 0, y: 0}
-    const ratio = devicePixelRatio || 1
-    let w: number
-    let h: number
+    const w = Data.tileScale * HexTile.c * 8
+    const h = Data.tileScale * HexTile.a * 10
+    export const center = {x: w / 2, y: h / 2}
     const msg = document.querySelector("#msg") as HTMLElement
+    const container = document.querySelector("#container") as HTMLElement
+    const pxRatio = devicePixelRatio || 1
+    let scale: number
+    let containerH: number
 
-    // resize the canvas to be the size of the window
     export function resize(): void {
-        w = innerWidth
-        h = innerHeight
-        center.x = w / 2
-        // because human eyes think the actual center is too low
-        center.y = h / 2.2
-        el.width = w * ratio
-        el.height = h * ratio
-        el.style.width = `${w}px`
-        el.style.height = `${h}px`
-        cx.scale(ratio, ratio)
+        containerH = innerHeight - parseInt(getComputedStyle(msg).height, 10)
+        container.style.height = `${containerH}px`
+        scale = Math.min(innerWidth / w, containerH / h)
+        el.width = scale * w * pxRatio
+        el.height = scale * h * pxRatio
+        el.style.width = `${scale * w}px`
+        el.style.height = `${scale * h}px`
+        cx.scale(scale * pxRatio, scale * pxRatio)
     }
 
     // clear the entirety of the canvas
