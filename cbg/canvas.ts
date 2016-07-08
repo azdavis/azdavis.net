1df4dd75e5e72c317fc3bc995aba40cb1625a7bd
azdavis <ariel.z.davis@icloud.com>
2016-07-07 21:08:29 -0400

    cbg: add body padding

--- cbg/canvas.ts
+++ cbg/canvas.ts
@@ -4,21 +4,26 @@ import HexTile from "./hex_tile"
 namespace Canvas {
     const el = document.querySelector("canvas") as HTMLCanvasElement
     export const cx = el.getContext("2d")
-
     const container = document.querySelector("#container") as HTMLElement
-    let containerH: number
     const btns = document.querySelector("#btns") as HTMLElement
-
     const pxRatio = devicePixelRatio || 1
-    let scale: number
+
+    function css(el: HTMLElement, prop: string): number {
+        return parseInt(getComputedStyle(el)[prop], 10)
+    }
 
     // resize the canvas to the given w and h while still fitting completely in
     // its container
     export function resize(w: number, h: number): void {
+        const p = 2 * css(document.body, "padding")
+        const b = css(btns, "height") + css(btns, "marginBottom")
+        const ch = innerHeight - p - b
+        const cw = innerWidth - p
+        const scale = Math.min(cw / w, ch / h)
+
         cx.clearRect(0, 0, w, h)
-        containerH = innerHeight - parseInt(getComputedStyle(btns).height, 10)
-        container.style.height = `${containerH}px`
-        scale = Math.min(innerWidth / w, containerH / h)
+        container.style.width = `${cw}px`
+        container.style.height = `${ch}px`
         el.width = scale * w * pxRatio
         el.height = scale * h * pxRatio
         el.style.width = `${scale * w}px`
