d14ca88745ed177708f7efc47853cf61437bd83d
azdavis <ariel.z.davis@icloud.com>
2016-07-14 19:29:32 -0400

    src/index: compute href names at compile time

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,2 @@
-const anchors = document.querySelectorAll("a") as NodeListOf<HTMLAnchorElement>
-const http = /https?:\/\//
-
-for (let i = 0; i < anchors.length; i++) {
-    if (anchors[i].href.match(http)) {
-        anchors[i].innerHTML = anchors[i].href.replace(http, "")
-    }
-}
-
 const now = document.querySelector("#now") as HTMLElement
-const d = new Date()
-
-now.innerHTML = String(d.getFullYear())
+now.innerHTML = String(new Date().getFullYear())
