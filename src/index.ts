4e907a96b2c7d24978e96b4ed06622fd33573817
azdavis <ariel.z.davis@icloud.com>
2016-07-15 01:38:19 -0400

    src/index: remove some .

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 console.log([
-    "the devtools have been opened, it would seem.",
-    "look around, or visit https://github.com/azdavis/azdavis.xyz.",
-    "there, you'll find the unminifed source code for this site.",
+    "the devtools have been opened, it would seem",
+    "look around, or visit https://github.com/azdavis/azdavis.xyz",
+    "there, you'll find the unminifed source code for this site",
 ].join("\n"))
