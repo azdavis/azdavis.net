6c43c26ff7ad99d59ee748920c16f9930c931d01
azdavis <ariel.z.davis@icloud.com>
2016-07-17 13:21:31 -0400

    src/rut/Rut: implement

--- src/rut/Rut.ts
+++ src/rut/Rut.ts
@@ -1,34 +1,70 @@
 import Canvas from "./Canvas"
 
 class Rut {
-    private static text = "rüt"
-    private static font = getComputedStyle(document.documentElement).fontFamily
+    private static text = "Rüt"
+    private static w = 5
+    private static h = 1
+    private static a = Math.atan(Rut.h / Rut.w)
 
+    public size: number
+    public dx: number
+    public dy: number
     private x: number
     private y: number
-
-    private speed: number
-    private size: number
     private color: string
 
-    public constructor(private angle: number) {
-        this.x = Canvas.center.x
-        this.y = Canvas.center.y
-        this.speed = 1
-        this.size = 20
-        this.color = "#f0f"
+    public constructor() {
+        this.reset()
+    }
+
+    public reset(): void {
+        this.color = `hsl(${Math.random() * 360}, 60%, 60%)`
+        this.size = 1
+
+        const a = Math.random() * 2 * Math.PI
+        if (0 <= a && a < Rut.a || 2 * Math.PI - Rut.a < a) {
+            this.dx = Rut.w
+            this.dy = Rut.w * Math.tan(a)
+        } else if (Rut.a <= a && a < Math.PI - Rut.a) {
+            this.dx = Rut.h / Math.tan(a)
+            this.dy = -Rut.h
+        } else if (Math.PI - Rut.a <= a && a < Math.PI + Rut.a) {
+            this.dx = -Rut.w
+            this.dy = Rut.w * Math.tan(a)
+        } else {
+            this.dx = -Rut.h / Math.tan(a)
+            this.dy = Rut.h
+        }
+
+        const s = 5 + Math.random() * 15
+        this.dx *= s
+        this.dy *= s
+        this.getXY()
     }
 
     public move(): void {
-        // TODO implement
-        this.x += this.angle * this.speed
+        this.size *= 1.01
+        this.getXY()
     }
 
     public draw(): void {
+        Canvas.cx.font = `bold small-caps ${this.size}px serif`
         Canvas.cx.fillStyle = this.color
-        Canvas.cx.font = `${this.size}px ${Rut.font}`
-
         Canvas.cx.fillText(Rut.text, this.x, this.y)
+    }
+
+    public isOutOfBounds(): boolean {
+        const hw = Rut.w * this.size / 2
+        const hh = Rut.h * this.size / 2
+        return this.x + hw < 0
+            || this.x - hw > Canvas.w
+            || this.y + hh < 0
+            || this.y - hh > Canvas.h
+    }
+
+    private getXY(): void {
+        this.x = Canvas.w / 2 + this.size * this.dx
+        this.y = Canvas.h / 2 + this.size * this.dy
     }
 }
 
