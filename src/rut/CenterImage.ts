003d3dd66f720312b912bade17349ade5c203fe2
azdavis <ariel.z.davis@icloud.com>
2016-07-18 13:19:04 -0400

    src/rut/CenterImage: improve comments

--- src/rut/CenterImage.ts
+++ src/rut/CenterImage.ts
@@ -2,12 +2,26 @@ import Canvas from "./Canvas"
 
 module CenterImage {
     export const el = new Image()
-    // HACK these dimensions are directly from the file
+    // HACK these dimensions are directly from a file
     const w = 219
     const h = 292
     let size = 0.001
 
     export function move(): void {
+        // the logistic growth function is the solution to the differential
+        // equation
+        //
+        //     df/dx = f(x)(1 - f(x))
+        //
+        // rearranging yields
+        //
+        //     df = (df/dx)dx = f(x)(1 - f(x))dx
+        //
+        // additionally, the f(x) value that is dx away from the current x
+        // value is about df away from the current f(x) value
+        //
+        //     f(x + dx) ≈ f(x) + df
+        //
         size += size * (1 - size) * 0.01
     }
 
