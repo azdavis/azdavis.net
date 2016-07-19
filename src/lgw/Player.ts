3b1758ff31415e1dae630de6e1a7aa0010b12007
azdavis <ariel.z.davis@icloud.com>
2016-07-19 01:57:37 -0400

    src/lgw/Player: inline a let

--- src/lgw/Player.ts
+++ src/lgw/Player.ts
@@ -51,8 +51,7 @@ class PlayerClass extends Sprite {
 
     // reset all dirs to false
     public stopMoving(): void {
-        let i
-        for (i in this.dirs) {
+        for (let i in this.dirs) {
             if (this.dirs[i]) {
                 this.dirs[i] = false
             }
