458de10b1411f95759835c823eb57f121a7086d6
azdavis <ariel.z.davis@icloud.com>
2016-07-21 14:36:22 -0400

    src/404: save a line

--- src/404.ts
+++ src/404.ts
@@ -1,6 +1,5 @@
 import {mk, prepend} from "./base/dom"
 
 const s = mk("#url")
-const u = location.host + location.pathname
-s.innerHTML = u
+s.innerHTML = location.host + location.pathname
 prepend(s)
