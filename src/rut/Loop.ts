acfd72ba71c5ded4783f8750f0056826c7d9fc9d
azdavis <ariel.z.davis@icloud.com>
2016-07-25 14:24:17 -0400

    src/rut/Loop: rename some vars, change an order

--- src/rut/Loop.ts
+++ src/rut/Loop.ts
@@ -2,8 +2,8 @@ import Canvas from "./Canvas"
 import CenterImage from "./CenterImage"
 import ZoomText from "./ZoomText"
 
-const zt: ZoomText[] = []
-const ztMax = 70
+const zts: ZoomText[] = []
+const ztsMax = 70
 
 // clear the canvas, maybe add a new ZoomText if there aren't already enough,
 // draw the CenterImage, then draw all the ZoomText (possibly on top of
@@ -11,17 +11,17 @@ const ztMax = 70
 function Loop(): void {
     Canvas.clear()
 
-    if (Math.random() < 0.05 && zt.length < ztMax) {
-        zt.push(new ZoomText())
+    if (Math.random() < 0.05 && zts.length < ztsMax) {
+        zts.push(new ZoomText())
+    }
+
+    for (let z of zts) {
+        z.move()
+        z.draw()
     }
 
     CenterImage.move()
     CenterImage.draw()
-
-    for (let i = 0; i < zt.length; i++) {
-        zt[i].move()
-        zt[i].draw()
-    }
 
     requestAnimationFrame(Loop)
 }
