7390b1ae4dcbae2c649967efc17c32b9c6713e07
azdavis <ariel.z.davis@icloud.com>
2016-07-28 14:41:25 -0400

    cbg/Canvas: clean up a bit

--- src/cbg/Canvas.ts
+++ src/cbg/Canvas.ts
@@ -11,23 +11,24 @@ module Canvas {
 
     const pxRatio = devicePixelRatio || 1
     const i = x => parseInt(x, 10)
+    const g = getComputedStyle
 
     // resize the canvas to the given w and h while still fitting completely in
     // its container
     export function resize(w: number, h: number): void {
-        const p = i(getComputedStyle(document.body).paddingTop)
-                + i(getComputedStyle(document.body).paddingBottom)
-        const b = i(getComputedStyle(btns).height)
-                + i(getComputedStyle(btns).marginBottom)
+        const p = i(g(document.body).paddingTop)
+                + i(g(document.body).paddingBottom)
+        const b = i(g(btns).height)
+                + i(g(btns).marginBottom)
         const ch = innerHeight - p - b
         const cw = innerWidth - p
         const scale = Math.min(cw / w, ch / h)
 
         cx.clearRect(0, 0, w, h)
-        container.style.width = `${cw}px`
-        container.style.height = `${ch}px`
         el.width = scale * w * pxRatio
         el.height = scale * h * pxRatio
+        container.style.width = `${cw}px`
+        container.style.height = `${ch}px`
         el.style.width = `${scale * w}px`
         el.style.height = `${scale * h}px`
         cx.scale(scale * pxRatio, scale * pxRatio)
