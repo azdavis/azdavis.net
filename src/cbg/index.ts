47ba5f37a360cb40d873ac46f251c7eba47fde87
azdavis <ariel.z.davis@icloud.com>
2016-07-29 01:25:08 -0400

    cbg/index: do some things for mobile

--- src/cbg/index.ts
+++ src/cbg/index.ts
@@ -2,14 +2,21 @@ import {$, append, div} from "../base/dom"
 import Board from "./Board"
 import Canvas from "./Canvas"
 
+const mobile = "ontouchend" in window
+
 $("#noscript").remove()
 onresize = Board.draw
 
 function makeDrawer(type: string): void {
     const el = div()
     el.innerHTML = type
-    el.onclick = () => {
+    function draw(): void {
         Board.drawNew(type)
+    }
+    if (mobile) {
+        el.ontouchend = draw
+    } else {
+        el.onclick = draw
     }
     append(el, Canvas.btns)
 }
@@ -22,6 +29,8 @@ append(Canvas.container)
 Board.drawNew("regular")
 
 // HACK makes :active work on iOS
-// tslint:disable:no-empty
-document.documentElement.ontouchend = () => {}
-// tslint:enable:no-empty
+if (mobile) {
+    // tslint:disable:no-empty
+    document.documentElement.ontouchend = () => {}
+    // tslint:enable:no-empty
+}
