c8aee3d1c737fff0831935d342b78605ccb0efcd
azdavis <ariel.z.davis@icloud.com>
2016-07-29 02:05:34 -0400

    lgw: change some caps, use some mono

--- src/lgw/index.ts
+++ src/lgw/index.ts
@@ -112,9 +112,9 @@ onblur = () => {
 // show an error or a "begin" message, depending on mobility of device
 const msg = $("#msg")
 if ("ontouchend" in window) {
-    msg.innerHTML = "a physical keyboard is required"
+    msg.innerHTML = "A physical keyboard is required"
 } else {
-    msg.innerHTML = "press ENTER to begin"
+    msg.innerHTML = "Press <code>enter</code> to begin"
     msg.className = "green"
 }
 
