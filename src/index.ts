c2acb7673d25810d7211376ade80e5e53195e1dc
azdavis <ariel.z.davis@icloud.com>
2016-08-14 17:13:34 -0400

    add comic-sans

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
+if (location.search.match("comic-sans")) {
+    document.documentElement.style.fontFamily =
+        "'Comic Sans MS', 'Comic Sans', sans-serif"
+}
+
 console.log("source: https://github.com/azdavis/azdavis.xyz")
