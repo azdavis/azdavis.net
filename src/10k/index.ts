d4d0e24dc7170e428b7e58123464f30cc9cdfa9b
azdavis <ariel.z.davis@icloud.com>
2016-08-30 10:27:53 -0400

    have less precision

--- src/10k/index.ts
+++ src/10k/index.ts
@@ -3,6 +3,6 @@ const load = document.querySelector("#load") as HTMLElement
 addEventListener("DOMContentLoaded", () => {
     load.style.display = "block"
     load.innerHTML = `The benefits are plain to see: this page took ${
-        (performance.now() / 1000).toFixed(3)
+        (performance.now() / 1000).toFixed(2)
     } seconds to load. That's probably a pretty good speed for your network.`
 }, false)
