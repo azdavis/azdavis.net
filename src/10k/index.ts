754ea259af45517a4a0c5e83822565676cd378f4
azdavis <ariel.z.davis@icloud.com>
2016-08-30 10:26:51 -0400

    use seconds, not ms

--- src/10k/index.ts
+++ src/10k/index.ts
@@ -3,6 +3,6 @@ const load = document.querySelector("#load") as HTMLElement
 addEventListener("DOMContentLoaded", () => {
     load.style.display = "block"
     load.innerHTML = `The benefits are plain to see: this page took ${
-        performance.now().toFixed(3)
-    } ms to load. That's probably a pretty good speed for your network.`
+        (performance.now() / 1000).toFixed(3)
+    } seconds to load. That's probably a pretty good speed for your network.`
 }, false)
