d13846aeaa007f7ddd0d72a5d4811fa87ad2545f
azdavis <ariel.z.davis@icloud.com>
2016-09-02 15:03:02 -0400

    use onload

--- src/10k/index.ts
+++ src/10k/index.ts
@@ -1,8 +1,8 @@
 const load = document.querySelector("#load") as HTMLElement
 
-addEventListener("DOMContentLoaded", () => {
+onload = () => {
     load.style.display = "block"
     load.innerHTML = `The benefits are plain to see: this page took ${
         (performance.now() / 1000).toFixed(2)
     } seconds to load. That's probably a pretty good speed for your network.`
-}, false)
+}
