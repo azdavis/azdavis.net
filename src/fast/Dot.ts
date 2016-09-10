d4070115b27a2ca09b3733cefcd2b272ac962064
azdavis <ariel.z.davis@icloud.com>
2016-09-10 16:20:26 -0400

    make dot size scale with speed

--- src/fast/Dot.ts
+++ src/fast/Dot.ts
@@ -1,15 +1,15 @@
 import Canvas from "./Canvas"
 
 class Dot {
-    private static dim = 3
+    private speed: number
+    private size: number
     private x: number
     private y: number
-    private speed: number
 
     // call reset(), but then set x to somewhere in the canvas
     public constructor() {
         this.reset()
-        this.x = Math.random() * (Canvas.w - Dot.dim)
+        this.x = Math.random() * (Canvas.w - this.size)
     }
 
     // set some canvas things common to all Dots
@@ -20,9 +20,10 @@ class Dot {
     // put this directly on the left side of the canvas, at a random y with a
     // random speed
     public reset(): void {
-        this.x = -Dot.dim
-        this.y = Math.random() * (Canvas.h - Dot.dim)
         this.speed = Math.random() * 5 + 5
+        this.size = this.speed * 0.7
+        this.x = -this.size
+        this.y = Math.random() * (Canvas.h - this.size)
     }
 
     // increase x by speed
@@ -35,8 +36,8 @@ class Dot {
         Canvas.cx.fillRect(
             this.x,
             this.y,
-            Dot.dim,
-            Dot.dim
+            this.size,
+            this.size
         )
     }
 
