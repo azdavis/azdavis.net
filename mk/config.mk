7b21c480b8e521f5643d0a070a632ada348f5ac9
azdavis <ariel.z.davis@icloud.com>
2016-11-01 01:10:57 -0400

    update BINARY

--- mk/config.mk
+++ mk/config.mk
@@ -1,4 +1,4 @@
 MAKEFLAGS += -rRs
 SHELL := PATH=node_modules/.bin:$(PATH) sh
 Q := &>/dev/null
-BINARY := src/favicon.png src/touch_icon.png
+BINARY := src/favicon.png src/touch_icon.png simple-js-game/starter.zip
