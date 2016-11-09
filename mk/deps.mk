5d7236ff7205e60bb9f48cd2ca3c4b4066b7b66d
azdavis <ariel.z.davis@icloud.com>
2016-11-09 17:19:06 -0500

    remove fast

--- mk/deps.mk
+++ mk/deps.mk
@@ -1,33 +1,19 @@
-src/index.css src/fast/index.css src/resume/index.css: \
+src/index.css src/resume/index.css: \
 	src/base/styl/a.styl
-src/index.css src/fast/index.css src/resume/index.css: \
+src/index.css src/resume/index.css: \
 	src/base/styl/a_dark.styl
-src/fast/index.css: \
-	src/base/styl/code.styl
-src/fast/index.css: \
-	src/base/styl/code_dark.styl
-src/404/index.css src/fast/index.css src/lgw/index.css src/resume/index.css: \
+src/404/index.css src/lgw/index.css src/resume/index.css: \
 	src/base/styl/h1_h2_h3.styl
-src/fast/index.css: \
-	src/base/styl/hr.styl
-src/index.css src/404/index.css src/fast/index.css src/resume/index.css: \
+src/index.css src/404/index.css src/resume/index.css: \
 	src/base/styl/main_text.styl
-src/index.css src/404/index.css src/fast/index.css src/resume/index.css: \
+src/index.css src/404/index.css src/resume/index.css: \
 	src/base/styl/main_text_dark.styl
-src/fast/index.css: \
-	src/base/styl/pre.styl
-src/fast/index.css: \
-	src/base/styl/pre_dark.styl
-src/index.js src/404/index.js src/fast/index.js src/resume/index.js: \
+src/index.js src/404/index.js src/resume/index.js: \
 	src/base/ts/dark.ts
-src/cbg/index.js src/fast/index.js src/lgw/index.js: \
+src/cbg/index.js src/lgw/index.js: \
 	src/base/ts/dom.ts
 src/cbg/index.js src/lgw/index.js: \
 	src/base/ts/is_mobile.ts
-src/fast/index.html: \
-	src/fast/index.md
-src/fast/index.js: \
-	$(wildcard src/fast/*.ts)
 src/cbg/index.js: \
 	$(wildcard src/cbg/*.ts)
 src/lgw/index.js: \
