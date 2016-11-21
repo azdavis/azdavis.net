b9170ff29c0950ded7d4ab2e508e7b6c2b68f67c
azdavis <ariel.z.davis@icloud.com>
2016-11-21 14:24:23 -0500

    remove lgw

--- mk/deps.mk
+++ mk/deps.mk
@@ -2,7 +2,7 @@ src/index.css src/resume/index.css: \
 	src/base/styl/a.styl
 src/index.css src/resume/index.css: \
 	src/base/styl/a_dark.styl
-src/lgw/index.css src/resume/index.css: \
+src/resume/index.css: \
 	src/base/styl/h1_h2_h3.styl
 src/index.css src/resume/index.css: \
 	src/base/styl/main_text.styl
@@ -10,9 +10,3 @@ src/index.css src/resume/index.css: \
 	src/base/styl/main_text_dark.styl
 src/index.js src/resume/index.js: \
 	src/base/ts/dark.ts
-src/lgw/index.js: \
-	src/base/ts/dom.ts
-src/lgw/index.js: \
-	src/base/ts/is_mobile.ts
-src/lgw/index.js: \
-	$(wildcard src/lgw/*.ts)
