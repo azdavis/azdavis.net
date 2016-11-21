e2cb4ca45792e9e791aecf5c9a1be03dabc392fb
azdavis <ariel.z.davis@icloud.com>
2016-11-21 14:14:09 -0500

    remove 404

--- mk/deps.mk
+++ mk/deps.mk
@@ -2,13 +2,13 @@ src/index.css src/resume/index.css: \
 	src/base/styl/a.styl
 src/index.css src/resume/index.css: \
 	src/base/styl/a_dark.styl
-src/404/index.css src/lgw/index.css src/resume/index.css: \
+src/lgw/index.css src/resume/index.css: \
 	src/base/styl/h1_h2_h3.styl
-src/index.css src/404/index.css src/resume/index.css: \
+src/index.css src/resume/index.css: \
 	src/base/styl/main_text.styl
-src/index.css src/404/index.css src/resume/index.css: \
+src/index.css src/resume/index.css: \
 	src/base/styl/main_text_dark.styl
-src/index.js src/404/index.js src/resume/index.js: \
+src/index.js src/resume/index.js: \
 	src/base/ts/dark.ts
 src/lgw/index.js: \
 	src/base/ts/dom.ts
--- src/404/index.ts
+++
@@ -1,1 +0,0 @@
-import "../base/ts/dark"
