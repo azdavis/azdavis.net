547a842c46bb6ea19c8f0c5250c9c1c07d13e62c
azdavis <ariel.z.davis@icloud.com>
2016-11-21 13:53:38 -0500

    remove cbg

--- mk/deps.mk
+++ mk/deps.mk
@@ -10,11 +10,9 @@ src/index.css src/404/index.css src/resume/index.css: \
 	src/base/styl/main_text_dark.styl
 src/index.js src/404/index.js src/resume/index.js: \
 	src/base/ts/dark.ts
-src/cbg/index.js src/lgw/index.js: \
+src/lgw/index.js: \
 	src/base/ts/dom.ts
-src/cbg/index.js src/lgw/index.js: \
+src/lgw/index.js: \
 	src/base/ts/is_mobile.ts
-src/cbg/index.js: \
-	$(wildcard src/cbg/*.ts)
 src/lgw/index.js: \
 	$(wildcard src/lgw/*.ts)
--- src/cbg/sprite.ts
+++
@@ -1,6 +0,0 @@
-abstract class Sprite {
-    protected fill: string
-    public abstract draw(x: number, y: number): void
-}
-
-export default Sprite
