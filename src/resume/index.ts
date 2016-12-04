20c92ef0015a04af8ba27cd91545bca6194cffa7
azdavis <ariel.z.davis@icloud.com>
2016-12-04 18:53:00 -0500

    use some kind of anti-spam strategy

--- src/resume/index.ts
+++ src/resume/index.ts
@@ -1,1 +1,20 @@
 import "../base/dark"
+
+const els = [
+    document.querySelector("#email") as HTMLAnchorElement
+  , document.querySelector("#tel") as HTMLAnchorElement
+  , document.querySelector("#gh") as HTMLAnchorElement
+]
+
+const data = [
+    "dWRlLnVtYy53ZXJkbmFAc2l2YWR6YQ=="
+  , "NjAwMS00MTUtODA5"
+  , "c2l2YWR6YS9tb2MuYnVodGln"
+].map(x => atob(x).split("").reverse().join(""))
+
+setTimeout(() => {
+    els[0].href = `mailto:${data[0]}`
+    els[1].href = `tel:${data[1]}`
+    els[2].href = `https://${data[2]}`
+    els.forEach((el, i) => el.setAttribute("data-print", data[i]))
+}, 200)
