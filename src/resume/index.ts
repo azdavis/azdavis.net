1a68d9cde38fefd94d321bd3f54235cfaaa4f277
azdavis <ariel.z.davis@icloud.com>
2016-12-10 00:54:22 -0500

    remove tel

--- src/resume/index.ts
+++ src/resume/index.ts
@@ -2,19 +2,16 @@ import "../base/dark"
 
 const els = [
     "email"
-  , "tel"
   , "gh"
 ].map(x => document.querySelector(`#${x}`) as HTMLAnchorElement)
 
 const data = [
     "dWRlLnVtYy53ZXJkbmFAc2l2YWR6YQ=="
-  , "NjAwMS00MTUtODA5"
   , "c2l2YWR6YS9tb2MuYnVodGln"
 ].map(x => atob(x).split("").reverse().join(""))
 
 setTimeout(() => {
     els[0].href = `mailto:${data[0]}`
-    els[1].href = `tel:${data[1]}`
-    els[2].href = `https://${data[2]}`
+    els[1].href = `https://${data[1]}`
     els.forEach((el, i) => el.setAttribute("data-print", data[i]))
 }, 200)
