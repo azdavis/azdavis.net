0ab23c5dd060d90475d1dd11d0e36723ce29461b
azdavis <ariel.z.davis@icloud.com>
2016-12-10 01:14:01 -0500

    "clean up" "obfuscation"

--- src/resume/index.ts
+++ src/resume/index.ts
@@ -1,17 +1,23 @@
 import "../base/dark"
 
-const els = [
-    "email"
-  , "gh"
-].map(x => document.querySelector(`#${x}`) as HTMLAnchorElement)
+function de(x: string): string {
+    return atob(x).split("").reverse().join("")
+}
 
-const data = [
-    "dWRlLnVtYy53ZXJkbmFAc2l2YWR6YQ=="
-  , "c2l2YWR6YS9tb2MuYnVodGln"
-].map(x => atob(x).split("").reverse().join(""))
+function a(x: string): HTMLAnchorElement {
+    return document.querySelector(`#${x}`) as HTMLAnchorElement
+}
+
+const email = de("dWRlLnVtYy53ZXJkbmFAc2l2YWR6YQ==")
+const gh = de("c2l2YWR6YS9tb2MuYnVodGln")
+const toDo = [
+    {el: a("email"), href: email, print: `mailto:${email}`}
+  , {el: a("gh"), href: gh, print: `https://${gh}`}
+]
 
 setTimeout(() => {
-    els[0].href = `mailto:${data[0]}`
-    els[1].href = `https://${data[1]}`
-    els.forEach((el, i) => el.setAttribute("data-print", data[i]))
+    toDo.forEach(x => {
+        x.el.href = x.href
+        x.el.setAttribute("data-print", x.print)
+    })
 }, 200)
