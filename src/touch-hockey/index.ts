20889f0dbcd15a1e04abfa0b9895697706901dfd
azdavis <ariel.z.davis@icloud.com>
2016-12-13 20:57:03 -0500

    implement moving of some circles

--- src/touch-hockey/index.ts
+++ src/touch-hockey/index.ts
@@ -1,4 +1,57 @@
 import "../base/dark"
 import Canvas from "./canvas"
+import ControlledCircle from "./controlled_circle"
 
-console.log(Canvas)
+const top = new ControlledCircle(Canvas.dim.width / 2, 100, "#d77")
+const bot = new ControlledCircle(Canvas.dim.width / 2, Canvas.dim.height - 100, "#77d")
+
+function render(): void {
+    Canvas.clear()
+    top.draw()
+    bot.draw()
+    requestAnimationFrame(render)
+}
+
+function start(id: string, x: number, y: number): void {
+    if (top.contains(x, y)) {
+        top.lastTouchID = id
+        top.moveTo(x, y)
+    } else if (bot.contains(x, y)) {
+        bot.lastTouchID = id
+        bot.moveTo(x, y)
+    }
+}
+
+function maintain(id: string, x: number, y: number): void {
+    if (top.lastTouchID === id) {
+        top.moveTo(x, y)
+    } else if (bot.lastTouchID === id) {
+        bot.moveTo(x, y)
+    }
+}
+
+function stop(id: string, x: number, y: number): void {
+    if (top.lastTouchID === id) {
+        top.stop()
+    } else if (bot.lastTouchID === id) {
+        bot.stop()
+    }
+}
+
+function all(
+    f: (id: string, x: number, y: number) => void
+): (e: TouchEvent) => void {
+    return e => {
+        e.preventDefault()
+        for (const {identifier, pageX, pageY} of e.changedTouches as any) {
+            f(identifier, pageX * Canvas.dim.scale, pageY * Canvas.dim.scale)
+        }
+    }
+}
+
+if ("ontouchend" in window) {
+    ontouchstart = all(start)
+    ontouchmove = all(maintain)
+    ontouchend = ontouchcancel = all(stop)
+    requestAnimationFrame(render)
+}
