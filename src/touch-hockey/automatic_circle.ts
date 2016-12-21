a665f1a917ad8642437bf3cdd93cf758199d841d
azdavis <ariel.z.davis@icloud.com>
2016-12-21 18:14:11 -0500

    add collide

--- src/touch-hockey/automatic_circle.ts
+++ src/touch-hockey/automatic_circle.ts
@@ -1,5 +1,18 @@
+import angle from "./angle"
 import Circle from "./circle"
+import distance from "./distance"
+import tau from "./tau"
 
-class AutomaticCircle extends Circle {}
+class AutomaticCircle extends Circle {
+    collide(other: Circle): void {
+        const phi = angle(other.y - this.y, other.x - this.x)
+        const a = other.speed * Math.cos(other.angle - phi)
+        const b = this.speed * Math.sin(this.angle - phi)
+        const x = a * Math.cos(phi) + b * Math.cos(phi * tau / 4)
+        const y = a * Math.sin(phi) + b * Math.sin(phi * tau / 4)
+        this.speed = distance(x, y)
+        this.angle = angle(x, y)
+    }
+}
 
 export default AutomaticCircle
