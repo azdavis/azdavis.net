329829234fa054ab07acad54942199959f98eb3b
azdavis <ariel.z.davis@icloud.com>
2016-12-21 18:13:49 -0500

    don't use dumbass access modifiers

--- src/touch-hockey/circle.ts
+++ src/touch-hockey/circle.ts
@@ -3,18 +3,18 @@ import distance from "./distance"
 import tau from "./tau"
 
 class Circle {
-    protected x: number
-    protected y: number
-    protected radius: number
-    protected xmin: () => number
-    protected xmax: () => number
-    protected ymin: () => number
-    protected ymax: () => number
-    protected color: string
-    protected angle: number
-    protected speed: number
+    x: number
+    y: number
+    radius: number
+    xmin: () => number
+    xmax: () => number
+    ymin: () => number
+    ymax: () => number
+    color: string
+    angle: number
+    speed: number
 
-    public constructor(
+    constructor(
         x: number
       , y: number
       , radius: number
@@ -35,7 +35,7 @@ class Circle {
         this.stop()
     }
 
-    public draw(): void {
+    draw(): void {
         Canvas.cx.fillStyle = this.color
         Canvas.cx.beginPath()
         Canvas.cx.arc(this.x, this.y, this.radius, 0, tau)
@@ -43,17 +43,17 @@ class Circle {
         Canvas.cx.fill()
     }
 
-    public stop(): void {
+    stop(): void {
         this.angle = 0
         this.speed = 0
     }
 
-    public overlaps(other: Circle): boolean {
+    overlaps(other: Circle): boolean {
         return distance(other.x - this.x, other.y - this.y)
              < other.radius + this.radius
     }
 
-    public contains(x: number, y: number): boolean {
+    contains(x: number, y: number): boolean {
         return distance(x - this.x, y - this.y)
              < this.radius
     }
--- src/touch-hockey/controlled_circle.ts
+++ src/touch-hockey/controlled_circle.ts
@@ -4,17 +4,17 @@ import distance from "./distance"
 import keepInBounds from "./keep_in_bounds"
 
 class ControlledCircle extends Circle {
-    protected id: string
+    id: string
 
-    public controlWith(id: string): void {
+    controlWith(id: string): void {
         this.id = id
     }
 
-    public isControlledBy(id: string): boolean {
+    isControlledBy(id: string): boolean {
         return this.id === id
     }
 
-    public moveTo(x: number, y: number): void {
+    moveTo(x: number, y: number): void {
         const boundedX = keepInBounds(this.xmin(), x, this.xmax())
         const boundedY = keepInBounds(this.ymin(), y, this.ymax())
         const dx = boundedX - this.x
