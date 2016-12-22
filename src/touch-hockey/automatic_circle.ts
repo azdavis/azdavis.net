a4573afa9cbbd600e5ccb93f1b31220402a358bc
azdavis <ariel.z.davis@icloud.com>
2016-12-22 17:43:16 -0500

    fix a typo

--- src/touch-hockey/automatic_circle.ts
+++ src/touch-hockey/automatic_circle.ts
@@ -29,8 +29,8 @@ class AutomaticCircle extends Circle {
         const phi = angle(other.y - this.y, other.x - this.x)
         const a = other.speed * Math.cos(other.angle - phi)
         const b = this.speed * Math.sin(this.angle - phi)
-        const x = a * Math.cos(phi) + b * Math.cos(phi * tau / 4)
-        const y = a * Math.sin(phi) + b * Math.sin(phi * tau / 4)
+        const x = a * Math.cos(phi) + b * Math.cos(phi + tau / 4)
+        const y = a * Math.sin(phi) + b * Math.sin(phi + tau / 4)
         this.speed = distance(x, y)
         this.angle = angle(x, y)
     }
