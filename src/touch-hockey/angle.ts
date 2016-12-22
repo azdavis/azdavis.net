abb904fa2e22752ed0cb7790e54857667a87656d
azdavis <ariel.z.davis@icloud.com>
2016-12-22 15:27:16 -0500

    never return NaN

--- src/touch-hockey/angle.ts
+++ src/touch-hockey/angle.ts
@@ -2,7 +2,9 @@ import tau from "./tau"
 
 function angle(dx: number, dy: number): number {
     const a = Math.atan(dy / dx)
-    return dx < 0 ? tau / 2 + a : a
+    return isNaN(a) ? 0
+         : dx < 0 ? tau / 2 + a
+         : a
 }
 
 export default angle
