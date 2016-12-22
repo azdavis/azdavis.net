e6710978a07bfcca91731e5f9f2ab6af67580f79
azdavis <ariel.z.davis@icloud.com>
2016-12-22 15:08:23 -0500

    do not shadow

--- src/touch-hockey/angle.ts
+++ src/touch-hockey/angle.ts
@@ -1,8 +1,8 @@
 import tau from "./tau"
 
 function angle(dx: number, dy: number): number {
-    const angle = Math.atan(dy / dx)
-    return dx < 0 ? tau / 2 + angle : angle
+    const a = Math.atan(dy / dx)
+    return dx < 0 ? tau / 2 + a : a
 }
 
 export default angle
