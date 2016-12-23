7d4bd779f933a3988a2bca72ac066b39aa6fcc15
azdavis <ariel.z.davis@icloud.com>
2016-12-23 16:22:31 -0500

    add move

--- src/touch-hockey/automatic_circle.ts
+++ src/touch-hockey/automatic_circle.ts
@@ -6,6 +6,18 @@ import tau from "./tau"
 
 class AutomaticCircle extends Circle {
     move(): void {
+        const dx = this.speed * Math.cos(this.angle)
+        const dy = this.speed * Math.sin(this.angle)
+        const newx = keepInBounds(this.xmin(), this.x + dx, this.xmax())
+        const newy = keepInBounds(this.ymin(), this.y + dy, this.ymax())
+        if (newx === this.xmin() || newx === this.xmax()) {
+            this.angle = tau / 2 - this.angle
+        }
+        if (newy === this.ymin() || newy === this.ymax()) {
+            this.angle *= -1
+        }
+        this.x = newx
+        this.y = newy
     }
 
     collideWith(other: Circle): void {
