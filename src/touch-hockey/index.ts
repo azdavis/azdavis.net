da0222ad41395a35ebaedb25933b703eb9fdc9ce
azdavis <ariel.z.davis@icloud.com>
2017-01-05 14:06:12 -0500

    use requestAnimationFrame

--- src/touch-hockey/index.ts
+++ src/touch-hockey/index.ts
@@ -6,7 +6,7 @@ const msg = document.querySelector("#msg") as HTMLElement
 if ("ontouchend" in window) {
     msg.remove()
     Touches.setup()
-    Game.render()
+    requestAnimationFrame(Game.render)
 } else {
     msg.innerHTML = "A touchscreen is required"
 }
