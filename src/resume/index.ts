ce0b4bead7676fabc09cbd07bb8924c7c7442f57
Ariel Davis <ariel.z.davis@icloud.com>
2017-02-18 17:36:22 -0500

    do not have newline

--- src/resume/index.ts
+++ src/resume/index.ts
@@ -10,8 +10,8 @@ const printable = (x: string): string =>
     (x.match(/(mailto:|https?:\/\/|tel:)(.+[^\/]).*/) as any)[2]
 
 setTimeout(() => {
-    $("#email").href = decode("dWRlLnVtYy53ZXJkbmFAc2l2YWR6YTpvdGxpYW0K")
-    $("#gh").href = decode("c2l2YWR6YS9tb2MuYnVodGlnLy86c3B0dGgK")
+    $("#email").href = decode("dWRlLnVtYy53ZXJkbmFAc2l2YWR6YTpvdGxpYW0=")
+    $("#gh").href = decode("c2l2YWR6YS9tb2MuYnVodGlnLy86c3B0dGg=")
     $("#tel").href = decode("NjAwMS00MTUtODA5OmxldA==")
     for (const x of $$("a")) {
         x.setAttribute("data-print", printable(x.href))
