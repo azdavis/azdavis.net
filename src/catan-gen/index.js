64e5f50fc8c36b173e911a54a4a1ed0fa361701b
Ariel Davis <ariel.z.davis@icloud.com>
2017-02-19 15:20:17 -0500

    add render

--- src/catan-gen/index.js
+++ src/catan-gen/index.js
@@ -1,3 +1,17 @@
+import Canvas from "./canvas"
 import RegularBoard from "./regular-board"
 
 console.log(RegularBoard)
+
+const x = () => Canvas.dim.width / 2
+const y = () => Canvas.dim.height / 2
+const r = () =>
+    // 20 / 21 / sqrt(3)
+    Math.min(Canvas.dim.width / 2, Canvas.dim.height * 0.549857399) - 20
+
+function render() {
+    Canvas.clear()
+    RegularBoard.draw(x(), y(), r())
+    setTimeout(render, 1000)
+}
+render()
