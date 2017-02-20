cd5cf55bb4fc2f0e3b436c0f027855f31a49291f
Ariel Davis <ariel.z.davis@icloud.com>
2017-02-19 23:25:00 -0500

    draw dots

--- src/catan-gen/resource-hex.js
+++ src/catan-gen/resource-hex.js
@@ -13,10 +13,16 @@ class ResourceHex {
         if (this.number === null) {
             return
         }
+        const dots = NumberDots[this.number]
+        const dotR = r / 40
+        const dotMid = (dots - 1) / 2
         Canvas.drawCircle(x, y, r / 2.5, "#ddd")
         Canvas.cx.font = `${r / 35}em sans-serif`
-        Canvas.cx.fillStyle = NumberDots[this.number] === 5 ? "#d22" : "#222"
-        Canvas.cx.fillText(String(this.number), x, y)
+        Canvas.cx.fillStyle = dots === 5 ? "#d22" : "#222"
+        Canvas.cx.fillText(String(this.number), x, y - dotR)
+        for (let i = 0; i < dots; i++) {
+            Canvas.drawCircle(x + (i - dotMid) * dotR * 3, y + r / 4.5, dotR)
+        }
     }
 }
 
