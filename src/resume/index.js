f6c6018599160a9fceaca5d3e4fbd90dac73b404
Ariel Davis <ariel.z.davis@icloud.com>
2017-03-04 13:37:51 -0500

    rewrite

--- src/resume/index.js
+++ src/resume/index.js
@@ -1,15 +1,16 @@
 import "../base/dark"
 
-const $ = x => document.querySelector(x)
-const $$ = x => document.querySelectorAll(x)
+const gid = x => document.getElementById(x)
 const decode = x => atob(x).split("").reverse().join("")
 const printable = x => x.match(/(mailto:|https?:\/\/|tel:)(.+[^\/]).*/)[2]
 
 setTimeout(() => {
-    $("#email").href = decode("dWRlLnVtYy53ZXJkbmFAc2l2YWR6YTpvdGxpYW0=")
-    $("#gh").href = decode("c2l2YWR6YS9tb2MuYnVodGlnLy86c3B0dGg=")
-    $("#tel").href = decode("NjAwMS00MTUtODA5OmxldA==")
-    for (const x of $$("a")) {
+    gid("email").href = decode("dWRlLnVtYy53ZXJkbmFAc2l2YWR6YTpvdGxpYW0=")
+    gid("gh").href = decode("c2l2YWR6YS9tb2MuYnVodGlnLy86c3B0dGg=")
+    gid("tel").href = decode("NjAwMS00MTUtODA5OmxldA==")
+    const links = document.querySelectorAll("a")
+    for (let i = 0; i < links.length; i++) {
+        const x = links[i]
         x.setAttribute("data-print", printable(x.href))
     }
 }, 500)
