9af7cc7e5ab5b36d00d1d925796a59ef04374b0f
Ariel Davis <ariel.z.davis@icloud.com>
2017-03-04 13:27:13 -0500

    update deps

--- mk/deps.mk
+++ mk/deps.mk
@@ -1,2 +1,4 @@
+src/catan-gen/index.c.js: \
+	$(shell find src/catan-gen -name "*.js" -not -name "*.c.js")
 src/touch-hockey/index.c.js: \
 	$(shell find src/touch-hockey -name "*.js" -not -name "*.c.js")
