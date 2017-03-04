d94675f53f8512aa8c42cedc1b5f7fb103d4e87f
Ariel Davis <ariel.z.davis@icloud.com>
2017-03-04 13:45:39 -0500

    rewrite again

--- src/resume/index.js
+++ src/resume/index.js
@@ -1,14 +1,18 @@
 import "../base/dark"
 
-const gid = x => document.getElementById(x)
 const decode = x => atob(x).split("").reverse().join("")
 const printable = x => x.match(/(mailto:|https?:\/\/|tel:)(.+[^\/]).*/)[2]
+const todo = {
+    email: "dWRlLnVtYy53ZXJkbmFAc2l2YWR6YTpvdGxpYW0=",
+    gh: "c2l2YWR6YS9tb2MuYnVodGlnLy86c3B0dGg=",
+    tel: "NjAwMS00MTUtODA5OmxldA=="
+}
+const links = document.querySelectorAll("a")
 
 setTimeout(() => {
-    gid("email").href = decode("dWRlLnVtYy53ZXJkbmFAc2l2YWR6YTpvdGxpYW0=")
-    gid("gh").href = decode("c2l2YWR6YS9tb2MuYnVodGlnLy86c3B0dGg=")
-    gid("tel").href = decode("NjAwMS00MTUtODA5OmxldA==")
-    const links = document.querySelectorAll("a")
+    for (const x in todo) {
+        document.getElementById(x).href = decode(todo[x])
+    }
     for (let i = 0; i < links.length; i++) {
         const x = links[i]
         x.setAttribute("data-print", printable(x.href))
