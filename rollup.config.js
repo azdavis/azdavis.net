189796d7feff6fe4d84c8283c19fd83c4b0c0594
Ariel Davis <ariel.z.davis@icloud.com>
2017-03-04 21:19:18 -0500

    add eslint

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import babel from "rollup-plugin-babel"
+import eslint from "rollup-plugin-eslint"
 
 const options = {
     entry: "index.js",
     dest: "index.c.js",
     format: "iife",
-    plugins: [babel()]
+    plugins: [eslint({throwError: true}), babel()]
 }
 
 export default options
