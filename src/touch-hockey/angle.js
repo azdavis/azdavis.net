642ec71ee0f43105b87c10b0d96b94302ea34d91
Ariel Davis <ariel.z.davis@icloud.com>
2017-07-02 15:16:49 -0400

    remove some tabs

--- src/touch-hockey/angle.js
+++ src/touch-hockey/angle.js
@@ -2,9 +2,7 @@ import tau from "./tau"
 
 function angle(dx, dy) {
 	const a = Math.atan(dy / dx)
-	return isNaN(a) ? 0
-		: dx < 0 ? tau / 2 + a
-		: a
+	return isNaN(a) ? 0 : dx < 0 ? tau / 2 + a : a
 }
 
 export default angle
