20b34ee84b71983adedb389af03a6a985402c5ee
Ariel Davis <ariel.z.davis@icloud.com>
2017-08-08 09:36:06 -0400

    remove keybase.txt

--- mk/config.mk
+++ mk/config.mk
@@ -1,3 +1,3 @@
 MAKEFLAGS += -rRs
 SHELL := PATH=$(PWD)/node_modules/.bin:$(PATH) sh
-BINARY := build/favicon.png build/touch-icon.png
+BINARY := build/favicon.png build/touch-icon.png build/keybase.txt
