e68aee5c54e22c65cd63251df68f886c90c0e744
Ariel Davis <ariel.z.davis@icloud.com>
2017-08-22 11:58:48 -0400

    do not use BINARY

--- mk/config.mk
+++ mk/config.mk
@@ -1,5 +1,4 @@
 SHELL = PATH=$(PWD)/node_modules/.bin:$(PATH) sh
-BINARY = build/favicon.png build/touch-icon.png build/keybase.txt
 
 .SILENT:
 .SUFFIXES:
--- mk/misc.mk
+++ mk/misc.mk
@@ -1,10 +1,18 @@
-build/google827af1fbb442e5a9.html:
+build/favicon.png: src/favicon.png
 	echo $@ && mkdir -p $(dir $@)
-	printf "google-site-verification: google827af1fbb442e5a9.html" > $@
+	cp $< $@
 
-$(BINARY):
+build/touch-icon.png: src/touch-icon.png
 	echo $@ && mkdir -p $(dir $@)
-	curl -fsSLo $@ "https://azdavis.xyz/$(patsubst build/%,%,$@)"
+	cp $< $@
+
+build/keybase.txt: src/keybase.txt
+	echo $@ && mkdir -p $(dir $@)
+	cp $< $@
+
+build/google827af1fbb442e5a9.html: src/google827af1fbb442e5a9.html
+	echo $@ && mkdir -p $(dir $@)
+	cp $< $@
 
 node_modules:
 	echo $@
