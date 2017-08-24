3b370d67dafbdbaef7e14736900738b95a6a7ef4
Ariel Davis <ariel.z.davis@icloud.com>
2017-08-24 14:48:25 -0400

    use PRECIOUS for all actual files

--- mk/config.mk
+++ mk/config.mk
@@ -3,4 +3,11 @@ N = $(PWD)/node_modules/.bin/
 .SILENT:
 .SUFFIXES:
 .PHONY: all clean test git-ok netlify upload setup deploy
-.PRECIOUS: build/%.css build/%.js
+.PRECIOUS: \
+	build/%.html \
+	build/%.css \
+	build/%.js \
+	build/% \
+	node_modules \
+	.git/hooks/pre-commit \
+	.git/hooks/post-checkout
