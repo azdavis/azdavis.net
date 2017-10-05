02fc7a949350db7336943506f395ab4f86c2ea57
Ariel Davis <ariel.z.davis@icloud.com>
2017-10-05 13:09:44 -0400

    care less about scrapers

--- src/resume/index.js
+++ src/resume/index.js
@@ -1,24 +1,9 @@
 import "/src/_base/dark"
 
-const decode = x => atob(x).split("").reverse().join("")
 const printable = x => x.match(/(mailto:|https?:\/\/|tel:)(.+[^\/]).*/)[2]
-const todo = {
-	email: "dWRlLnVtYy53ZXJkbmFAc2l2YWR6YTpvdGxpYW0=",
-	gh: "c2l2YWR6YS9tb2MuYnVodGlnLy86c3B0dGg=",
-	tel: "NjAwMS00MTUtODA5OmxldA=="
-}
 const links = document.querySelectorAll("a")
 
 for (let i = 0; i < links.length; i++) {
 	const x = links[i]
 	x.setAttribute("data-print", printable(x.href))
-}
-for (const x in todo) {
-	const el = document.getElementById(x)
-	const href = decode(todo[x])
-	el.setAttribute("data-print", printable(href))
-	el.onclick = e => {
-		e.preventDefault()
-		location.assign(href)
-	}
 }
