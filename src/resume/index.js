b2602bbdf310a57243416f5100f737e6515be797
Ariel Davis <ariel.z.davis@icloud.com>
2017-10-24 18:59:20 -0400

    rm data-print

--- src/resume/index.js
+++
@@ -1,7 +0,0 @@
-const printable = x => x.match(/(mailto:|https?:\/\/|tel:)(.+[^\/])/)[2]
-const links = document.querySelectorAll("a")
-
-for (let i = 0; i < links.length; i++) {
-	const x = links[i]
-	x.setAttribute("data-print", printable(x.href))
-}
