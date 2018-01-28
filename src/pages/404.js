19fd1204e991e268ee0a759880ba51d7086b843a
Ariel Davis <ariel.z.davis@icloud.com>
2018-01-28 13:19:46 -0500

    improve indent

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,9 @@
 import React from "react"
 import "./404.css"
 
-const NotFoundPage = () =>
-	<div>
-		<h1>404 Not Found</h1>
-		<p>This page does not exist</p>
-	</div>
+const NotFoundPage = () => <div>
+	<h1>404 Not Found</h1>
+	<p>This page does not exist</p>
+</div>
 
 export default NotFoundPage
