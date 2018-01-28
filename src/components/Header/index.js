32a95d5a0731d92c08d712f9039b31ed8c4235e8
Ariel Davis <ariel.z.davis@icloud.com>
2018-01-28 12:48:10 -0500

    improve header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,6 @@ import React from "react"
 import Link from "gatsby-link"
 
 const Header = () =>
-	<h1><Link to="/">Gatsby</Link></h1>
+	<h1><Link to="/">azdavis.xyz</Link></h1>
 
 export default Header
