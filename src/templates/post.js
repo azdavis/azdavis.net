8e5498c552c06db4888e269607480b095617afbe
Ariel Davis <ariel.z.davis@icloud.com>
2018-01-30 13:05:47 -0500

    add link to / from posts

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Link from "gatsby-link"
 import timeago from "timeago.js"
 
 const now = timeago()
 export default ({data: {markdownRemark: {frontmatter, html}}}) => <div>
+	<Link to="/">home</Link>
 	<h1>{frontmatter.title}</h1>
 	<time dateTime={frontmatter.date}>{now.format(frontmatter.date)}</time>
 	<div dangerouslySetInnerHTML={{__html: html}}/>
