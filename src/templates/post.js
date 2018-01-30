d16623b9e7fa3cfb866db837415c1287106a89a3
Ariel Davis <ariel.z.davis@icloud.com>
2018-01-30 11:20:20 -0500

    use timeago

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,8 +1,10 @@
 import React from "react"
+import timeago from "timeago.js"
 
+const now = timeago()
 export default ({data: {markdownRemark: {frontmatter, html}}}) => <div>
 	<h1>{frontmatter.title}</h1>
-	<time dateTime={frontmatter.date}>{frontmatter.date}</time>
+	<time dateTime={frontmatter.date}>{now.format(frontmatter.date)}</time>
 	<div dangerouslySetInnerHTML={{__html: html}}/>
 </div>
 
