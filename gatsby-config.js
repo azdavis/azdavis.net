10d0fdb31e1fb77fc07b01fdf0864a2345dd658d
Ariel Davis <ariel.z.davis@icloud.com>
2018-01-29 20:10:53 -0500

    use new plugins in gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
 	siteMetadata: {
 		title: "man azdavis",
 	},
-	plugins: ["gatsby-plugin-react-helmet"],
+	plugins: [
+		"gatsby-plugin-react-helmet",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				path: `${__dirname}/src/posts`,
+				name: "markdown-pages",
+			}
+		},
+		"gatsby-transformer-remark",
+	],
 }
