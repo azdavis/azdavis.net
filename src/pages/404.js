e5b57349433c73c3ab3068f79db4ca4020e02022
Ariel Davis <ariel.z.davis@icloud.com>
2018-02-05 15:31:28 -0500

    add link to Home from 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Link from "gatsby-link"
 import Helmet from "react-helmet"
 
 export default ({data: {site}}) => <div>
 	<Helmet title={`404 Not Found - ${site.siteMetadata.title}`}/>
+	<Link to="/">Home</Link>
 	<h1>404 Not Found</h1>
 	<p>This page does not exist</p>
 </div>
