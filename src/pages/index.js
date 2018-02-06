c8744fdd0f9bef0cee8adfb0987efd4dc04d4c2f
Ariel Davis <ariel.z.davis@icloud.com>
2018-02-05 21:42:23 -0500

    add separate profiles page

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,19 +29,6 @@ export default () => <div>
 		azdavis is pursuing a major in computer science, with a minor in
 		Japanese studies, from Carnegie Mellon University.
 	</SecContent>
-	<SecTitle>PROFILES</SecTitle>
-	<SecContent><dl>
-		<dt><a href="https://github.com/azdavis">GitHub</a></dt>
-		<dd>A server for git remotes.</dd>
-		<dt><a href="https://www.reddit.com/user/azdavis">Reddit</a></dt>
-		<dd>A place for people to discuss things.</dd>
-		<dt><a href="https://news.ycombinator.com/user?id=azdavis">
-			Hacker News
-		</a></dt>
-		<dd>A place for hackers to discuss things.</dd>
-		<dt><a href="https://keybase.io/azdavis">Keybase</a></dt>
-		<dd>A place for verification of profile ownership.</dd>
-	</dl></SecContent>
 	<SecTitle>AUTHORS</SecTitle>
 	<SecContent>Written by David and Karen Davis.</SecContent>
 	<SecTitle>COPYRIGHT</SecTitle>
