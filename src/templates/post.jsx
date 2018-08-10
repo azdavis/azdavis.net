eb6c11eabb88f1d9311bd33cadc280830716b5f8
Ariel Davis <ariel.z.davis@icloud.com>
2018-08-09 23:38:50 -0700

    Reformat

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -5,18 +5,19 @@ import timeago from "timeago.js";
 
 const now = timeago();
 
-const RelativeTime = ({date}) =>
+const RelativeTime = ({date}) => (
   <time dateTime={date}>{now.format(date)}</time>
+);
 
 const mkPageTitle = (data) =>
-  `${data.markdownRemark.frontmatter.title} - ${data.site.siteMetadata.title}`
+  `${data.markdownRemark.frontmatter.title} - ${data.site.siteMetadata.title}`;
 
 export default ({data}) => (
   <div>
     <Helmet title={mkPageTitle(data)} />
     <Link to="/posts/">Posts</Link>
-    <h1>{data.markdownRemark.frontmatter.title}</h1>
-    (<RelativeTime date={data.markdownRemark.frontmatter.date} />)
+    <h1>{data.markdownRemark.frontmatter.title}</h1>(
+    <RelativeTime date={data.markdownRemark.frontmatter.date} />)
     <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}} />
   </div>
 );
