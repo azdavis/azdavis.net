c318ed36fabac32a7d21f992bbf693b8b2a9d8f0
Ariel Davis <ariel.z.davis@icloud.com>
2018-09-16 17:44:53 -0400

    Change languages

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -79,7 +79,7 @@ export default ({data}) => (
       </dd>
       <h2>Skills</h2>
       <dt>Languages</dt>
-      <dd>Standard ML, TypeScript, Rust, C, others</dd>
+      <dd>TypeScript, Rust, Standard ML, Go, C, others</dd>
       <dt>Markup</dt>
       <dd>HTML, CSS, LaTeX, Markdown</dd>
       <dt>Tools</dt>
