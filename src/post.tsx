e47a6eb3c00d518ee522604dd80a570ab1bbc16b
Ariel Davis <ariel.z.davis@icloud.com>
2021-06-02 00:56:13 -0700

    Clean up post

--- src/post.tsx
+++ src/post.tsx
@@ -5,21 +5,29 @@ import hl from "highlight.js";
 import katex from "remarkable-katex";
 import lean from "highlightjs-lean";
 
-interface Props {
-  title: string;
-  content: string;
-  date: Date;
-  lang: string;
-}
-
 hl.registerLanguage("lean", lean);
 
 function highlight(code: string, language: string): string {
   return hl.getLanguage(language) ? hl.highlight(code, { language }).value : "";
 }
 
-const markdown = new Remarkable({ highlight });
-markdown.use(katex);
+const md = new Remarkable({ highlight });
+md.use(katex);
+
+interface UnsafeHtml {
+  __html: string;
+}
+
+function renderMd(content: string): UnsafeHtml {
+  return { __html: md.render(content) };
+}
+
+interface Props {
+  title: string;
+  content: string;
+  date: Date;
+  lang: string;
+}
 
 export function Post({ title, content, date, lang }: Props): ReactElement {
   return (
@@ -28,7 +36,7 @@ export function Post({ title, content, date, lang }: Props): ReactElement {
         <a href="/">azdavis.xyz</a> • <DateShow lang={lang} date={date} />
       </div>
       <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: markdown.render(content) }} />
+      <div dangerouslySetInnerHTML={renderMd(content)} />
     </>
   );
 }
