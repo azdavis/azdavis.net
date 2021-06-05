0651b40ad1e32d9d7e03c3d8e0b6d0b17defb896
Ariel Davis <ariel.z.davis@icloud.com>
2021-06-05 15:49:32 -0700

    Use a JA desc

--- src/page.tsx
+++ src/page.tsx
@@ -3,6 +3,15 @@ import type { ReactNode, ReactElement } from "react";
 
 export type Style = "base" | "code" | "index" | "katex/katex.min";
 
+const translations = {
+  en: {
+    desc: "the personal website of azdavis",
+  },
+  ja: {
+    desc: "azdavisの個人的なウェブサイト",
+  },
+};
+
 interface Props {
   lang: Lang;
   title: string;
@@ -11,12 +20,13 @@ interface Props {
 }
 
 export function Page({ lang, title, styles, children }: Props): ReactElement {
+  const t = translations[lang];
   return (
     <html lang={lang}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="description" content="the personal website of azdavis" />
+        <meta name="description" content={t.desc} />
         <title>{title}</title>
         <link rel="icon" href="/favicon.png" />
         {styles.map((s) => (
