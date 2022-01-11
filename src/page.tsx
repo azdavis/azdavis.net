ef7413c1823dd7914e1fbe50e34819f8c0264b15
Ariel Davis <ariel.z.davis@icloud.com>
2022-01-10 23:39:48 -0800

    Make shorter

--- src/page.tsx
+++ src/page.tsx
@@ -4,12 +4,8 @@ import type { Lang } from "./lang";
 export type Style = "base" | "code" | "index" | "katex/katex.min" | "posts";
 
 const translations = {
-  en: {
-    desc: "the personal website of azdavis",
-  },
-  ja: {
-    desc: "azdavisの個人的なウェブサイト",
-  },
+  en: "the personal website of azdavis",
+  ja: "azdavisの個人的なウェブサイト",
 };
 
 interface Props {
@@ -20,14 +16,13 @@ interface Props {
 }
 
 export function Page({ lang, title, styles, children }: Props): ReactElement {
-  const t = translations[lang];
   return (
     <html lang={lang}>
       <head>
         <meta charSet="utf-8" />
         <title>{title}</title>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="description" content={t.desc} />
+        <meta name="description" content={translations[lang]} />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="mask-icon" href="/favicon.svg" color="#000000" />
         <link rel="alternate icon" type="image/png" href="/favicon.png" />
