57832ffff304ae8ddc083734691efb43b53ba48b
Ariel Davis <ariel.z.davis@icloud.com>
2022-01-15 14:32:44 -0800

    Add . to 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,6 +4,6 @@ export const error404 = (
   <Page lang="en" title="404 Not Found" styles={["base"]}>
     <a href="/">azdavis.net</a>
     <h1>404 Not Found</h1>
-    <p>This page does not exist</p>
+    <p>This page does not exist.</p>
   </Page>
 );
