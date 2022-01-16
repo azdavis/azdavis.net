37aa7a9358b2c4f9d5a1deb2e38c29d7fe4eb394
Ariel Davis <ariel.z.davis@icloud.com>
2022-01-15 16:42:59 -0800

    Just say RSS

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -18,14 +18,11 @@ export function postCmp(a: PostListItem, b: PostListItem): -1 | 1 {
     : -1;
 }
 
-const rssFeed = { en: "RSS feed", ja: "RSSフェード" };
-
 export function postsPage(lang: Lang, posts: PostListItem[]): ReactElement {
   const title = translations[lang];
   return (
     <Page lang={lang} title={title} styles={["base", "posts"]}>
-      <a href={root(lang)}>azdavis.net</a> •{" "}
-      <a href={feedUrl(lang)}>{rssFeed[lang]}</a>
+      <a href={root(lang)}>azdavis.net</a> • <a href={feedUrl(lang)}>RSS</a>
       <h1>{title}</h1>
       {posts.map(({ title, path, date }) => (
         <div key={title} className="post-list-item">
