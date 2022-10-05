2edc5d3cd168ac04ae22f46587ef8ddc94f974a7
Ariel Davis <ariel.z.davis@icloud.com>
2022-10-05 14:01:39 -0700

    Avoid self-closing />

--- src/util/e.ts
+++ src/util/e.ts
@@ -7,5 +7,7 @@ export function e(
     .map(([a, b]) => `${a}=${JSON.stringify(b)}`)
     .join(" ");
   const fst = attrString.length === 0 ? name : name + " " + attrString;
-  return body == null ? `<${fst} />` : `<${fst}>${body.join("")}</${name}>`;
+  // w3c html validate doesn't like self-closing tag syntax *shrug*
+  // https://google.github.io/styleguide/htmlcssguide.html#Document_Type
+  return body == null ? `<${fst}>` : `<${fst}>${body.join("")}</${name}>`;
 }
